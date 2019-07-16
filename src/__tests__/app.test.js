9600f8c28d632031b3723c23b46302dbf132a8b8
Colin <colinmaher77yahoo.com>
2019-07-15 17:26:33 -0700

    added travis ci and wrote first test + cleanup

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import App from '../App'
+
+
+describe("App component", () => {
+  test("Matches the snapshot", () => {
+    const app = create(<App />);
+    expect(app.toJSON()).toMatchSnapshot();
+  });
+});
