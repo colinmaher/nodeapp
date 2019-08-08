7474d52e1d665434ccd79d94ea39ee9d8fa954bf
Colin <colinmaher77yahoo.com>
2019-08-08 14:49:40 -0700

    file cleanup

--- server/testing/redisTest.js
+++ server/testing/redisTest.js
@@ -1,19 +1,23 @@
 const redis = require("redis");
 const bluebird = require("bluebird");
+require("dotenv").config();
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
-const client = redis.createClient(6380, process.env.REDISCACHEHOSTNAME, 
-    {auth_pass: process.env.REDISCACHEKEY, tls: {servername: process.env.REDISCACHEHOSTNAME}});
+const client = redis.createClient(6380, process.env.REDISCACHEHOSTNAME,
+  { auth_pass: process.env.REDISCACHEKEY, tls: { servername: process.env.REDISCACHEHOSTNAME } });
 
 client.on("error", function (err) {
-  console.log("Error " + err);
+  console.log(err);
+});
+client.on('connect', function () {
+  console.log('Redis client connected');
 });
 
 async function testCache() {
 
   // Connect to the Azure Cache for Redis over the SSL port using the key.
-      
+
   // Perform cache operations using the cache connection object...
   client.set("key", "value1", redis.print);
   // Simple PING command
@@ -22,19 +26,19 @@ async function testCache() {
 
   // Simple get and put of integral data types into the cache
   console.log("\nCache command: GET Message");
-  console.log("Cache response : " + await client.getAsync("Message"));    
+  console.log("Cache response : " + await client.getAsync("Message"));
 
   console.log("\nCache command: SET Message");
   console.log("Cache response : " + await client.setAsync("Message",
-      "Hello! The cache is working from Node.js!"));    
+    "Hello! The cache is working from Node.js!"));
 
   // Demonstrate "SET Message" executed as expected...
   console.log("\nCache command: GET Message");
-  console.log("Cache response : " + await client.getAsync("Message"));    
+  console.log("Cache response : " + await client.getAsync("Message"));
 
   // Get the client list, useful to see if connection list is growing...
   console.log("\nCache command: CLIENT LIST");
-  console.log("Cache response : " + await client.clientAsync("LIST"));    
+  console.log("Cache response : " + await client.clientAsync("LIST"));
 }
 
 testCache();
